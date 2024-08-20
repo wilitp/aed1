9928daec99bdec2a1a7b5050e6d58a9fc8467f6f
wilitp <guilledeipola@gmail.com>
2024-08-19 22:08:58 -0300

    Repaso haciendo hasta el labo 9

--- ayudantia2024/practico1.hs
+++ ayudantia2024/practico1.hs
@@ -1,4 +1,102 @@
-f :: Int -> Bool
-f n = any (\(i, j) -> i * j == n) aux
+-- Laboratorio 1
+--
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use foldr" #-}
+
+esCero :: Int -> Bool
+esCero x = x == 0
+
+esPositivo :: Int -> Bool
+esPositivo x = x > 0
+
+esVocal :: Char -> Bool
+esVocal = (`elem` "aeiou")
+
+valorAbsoluto :: Int -> Int
+valorAbsoluto x
+  | x >= 0 = x
+  | otherwise = -x
+
+-- Laboratorio 2
+
+paratodo :: [Bool] -> Bool
+paratodo [] = True
+paratodo (x : xs) = x && paratodo xs
+
+sumatoria :: [Int] -> Int
+sumatoria [] = 0
+sumatoria (x : xs) = x + sumatoria xs
+
+productoria :: [Int] -> Int
+productoria [] = 1
+productoria (x : xs) = x * productoria xs
+
+factorial :: Int -> Int
+factorial x = productoria [1 .. x]
+
+promedio :: [Int] -> Int
+promedio xs = div (sumatoria xs) (length xs)
+
+-- Laboratorio 3
+
+f3a :: [Int] -> Bool
+f3a [] = True
+f3a (x : xs) = x > 0 && f3a xs
+
+f3b :: Int -> [Int] -> Bool
+f3b _ [] = False
+f3b z (x : xs) = (z == x) && f3b z xs
+
+f3c xs ys = all (`elem` ys) xs
+
+f3d [] = True
+f3d [x] = True
+f3d (x : z : xs) = x == z && f3d (z : xs)
+
+-- Laboratorio 4
+
+f4a = productoria
+
+f4b = promedio
+
+f4c [] _ = True -- Ver "Rango vacio" en el digesto
+f4c _ [] = True -- Ver "Rango vacio" en el digesto
+-- Ahora se asume que ninguna de las dos listas es vacia
+f4c xs ys = foldr max (xs !! 1) xs < foldr min (ys !! 1) ys
+
+f4d :: Int -> Bool
+f4d n = any (\(i, j) -> i * j == n) aux
   where
-    aux = [(i, j) | i <- [1 .. n - 1], j <- [1 .. n - 1]]
+    aux = [(i, j) | i <- [2 .. n - 1], j <- [2 .. n - 1]]
+
+-- Laboratorio 5
+-- lo omito
+
+-- Laboratorio 6
+
+f6b n xs = n `elem` xs && all (n >) xs
+
+f6c xs = product $ filter even xs
+
+f6d xs = product [xs !! i | i <- indexes]
+  where
+    indexes = filter even [0 .. (length xs - 1)]
+
+-- Laboratorio 7
+-- lo omito
+
+-- Laboratorio 9
+-- Hago los que me parecen mas interesantes
+--
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+-- Observacion: si n es fib(i), entonces i <= n
+-- Entonces [1..n] me da el espacio de busqueda
+esFib n = any ((== n) . fib) [1 .. n]
+
+todosFib :: [Int] -> Bool
+todosFib = all esFib
