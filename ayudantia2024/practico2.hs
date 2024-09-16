0ba371f0fe3cf6952bb7a20a797934b9677877ee
wilitp <guilledeipola@gmail.com>
2024-09-16 12:24:48 -0300

    aproximacion de pi

--- ayudantia2024/practico2.hs
+++ ayudantia2024/practico2.hs
@@ -1,3 +1,5 @@
+import GHC.Float (powerFloat)
+
 -- 4.b)
 
 -- minimoElemento' :: [Int] -> Int
@@ -10,3 +12,7 @@ type Punto = (Float, Float)
 
 data Figura = Circulo Punto Float
   deriving (Eq)
+
+aproxPiTerm i = 4 * (powerFloat (-1) i / (2 * i + 1))
+
+aproxPi n = sum (map aproxPiTerm [0 .. n])
